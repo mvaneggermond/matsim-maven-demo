1384f67fd5a3bff1a9b0bb451f7f64baa608a433
van Eggermond Michael <michael.vaneggermond@fhnw.ch>
2021-02-09 20:52:39 +0100

    Added static / non-static example

--- src/main/java/matsim/MatsimMavenDemo.java
+++ src/main/java/matsim/MatsimMavenDemo.java
@@ -14,18 +14,39 @@ public class MatsimMavenDemo {
 
     public static void main(String[] args) {
         System.out.println("Running a demo"); // Display the string.
+        System.out.println("Static context"); // Display the string.
 
         try{
 
-            run();
+            runStaticContext();
 
         }catch(Exception e)
         {
             System.out.println("Error " + e);
         }
+        System.out.println("Initialized"); // Display the string.
+        try{
+
+            MatsimMavenDemo md = new MatsimMavenDemo();
+            md.runInitializedContext();
+        }catch(Exception e)
+        {
+            System.out.println("Error " + e);
+        }
+
+
     }
 
-    private static void run() throws FileNotFoundException, IOException
+    public MatsimMavenDemo()
+    {
+        super();
+    }
+
+    /*
+    * Works only in a static context: MatsimMavenDemo.class.getClassLoader().
+    * https://mkyong.com/java/java-getresourceasstream-in-static-method/
+    */
+    private static void runStaticContext() throws FileNotFoundException, IOException
     {
         // the stream holding the file content
         InputStream csvFile = MatsimMavenDemo.class.getClassLoader().getResourceAsStream("network.csv");
@@ -42,6 +63,34 @@ public class MatsimMavenDemo {
         Population population = scenario.getPopulation();
 
 
+    }
+
+    /*
+     * Initialized context: MatsimMavenDemo.class.getClassLoader().
+     * https://mkyong.com/java/java-getresourceasstream-in-static-method/
+     */
+    private void runInitializedContext() throws FileNotFoundException, IOException
+    {
+        // the stream holding the file content
+        InputStream csvFile = getClass().getClassLoader().getResourceAsStream("network.csv");
+
+        this.readCSV(csvFile);
+
+
+    }
+
+    private void readCSV(InputStream csvFile)
+    {
+        InputStreamReader input = new InputStreamReader(csvFile);
+        CSVParser csvParser = CSVFormat.EXCEL.withFirstRecordAsHeader().parse(input);
+        for (CSVRecord record : csvParser) {
+            String field_1 = record.get("from");
+            String field_2 = record.get("to");
+            String field_3 = record.get("cost");
+        }
+
+        Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        Population population = scenario.getPopulation();
     }
 
 }
